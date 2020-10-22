122ad73ee5121127a38e365f077fa9b5abcb83b4
Luka1008gardasevic <70768726+Luka1008gardasevic@users.noreply.github.com>
2020-10-22 13:48:45 +0200

    products

--- products.js
+++ products.js
@@ -1,183 +1,183 @@
 var productsData = {
-	"cart-items":[
+	"cartItems":[
 		{
 			"name":"Carrera",
-            "price":"78.99",
+            "price":"€78.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/carrera.jpg"
+			"imageUrl":"./img/Muske/carrera.jpg"
         },
         {
 			"name":"Hugo",
-            "price":"129.99",
+            "price":"€129.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/hugo.jpg"
+			"imageUrl":"./img/Muske/hugo.jpg"
         },
         {
 			"name":"Hugo Boss",
-            "price":"95.99",
+            "price":"€95.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/huhoboss.jpg"
+			"imageUrl":"./img/Muske/huhoboss.jpg"
         },
         {
 			"name":"Marc Jacobs",
-            "price":"109.99",
+            "price":"€109.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/marcjacobs.jpg"
+			"imageUrl":"./img/Muske/marcjacobs.jpg"
         },
         {
 			"name":"Polaroid",
-            "price":"69.99",
+            "price":"€69.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/polaroid.jpg"
+			"imageUrl":"./img/Muske/polaroid.jpg"
         },
         {
 			"name":"Tommi Hilfiger",
-            "price":"114.99",
+            "price":"€114.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/tommihilfiger.jpg"
+			"imageUrl":"./img/Muske/tommihilfiger.jpg"
         },
         {
 			"name":"SuperDry Blue",
-            "price":"79.99",
+            "price":"€79.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/superdry.jpg"
+			"imageUrl":"./img/Muske/superdry.jpg"
         },
         {
 			"name":"O'Neill",
-            "price":"49.99",
+            "price":"€49.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/oneill.jpg"
+			"imageUrl":"./img/Muske/oneill.jpg"
         },
         {
 			"name":"Police",
-            "price":"99.99",
+            "price":"€99.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/police.jpg"
+			"imageUrl":"./img/Muske/police.jpg"
         },
         {
 			"name":"Police Drop",
-            "price":"84.99",
+            "price":"€84.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/policedrop.jpg"
+			"imageUrl":"./img/Muske/policedrop.jpg"
         },
         {
 			"name":"RayBan",
-            "price":"119.99",
+            "price":"€119.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/rayban3.jpg"
+			"imageUrl":"./img/Muske/rayban3.jpg"
         },
         {
 			"name":"RB Blue",
-            "price":"89.99",
+            "price":"€89.99",
             "model":"muski",
-			"imageUrl":"/img/Muske/rayban4.jpg"
+			"imageUrl":"./img/Muske/rayban4.jpg"
         },
         {
 			"name":"Degrees North",
-            "price":"48.99",
+            "price":"€48.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/degreesnorth.jpg"
+			"imageUrl":"./img/Zenske/degreesnorth.jpg"
         },
         {
 			"name":"Etnia Barcelona",
-            "price":"54.99",
+            "price":"€54.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/degreesnorth.jpg"
+			"imageUrl":"./img/Zenske/etniabarcelona.jpg"
         },
         {
 			"name":"Fendi",
-            "price":"139.99",
+            "price":"€139.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/fendi.jpg"
+			"imageUrl":"./img/Zenske/fendi.jpg"
         },
         {
 			"name":"George V",
-            "price":"69.99",
+            "price":"€69.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/georgev.jpg"
+			"imageUrl":"./img/Zenske/georgev.jpg"
         },
         {
 			"name":"Jimmy Choo",
-            "price":"149.99",
+            "price":"€149.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/jimmichoo.jpg"
+			"imageUrl":"./img/Zenske/jimmichoo.jpg"
         },{
 			"name":"Marc Jacobs",
-            "price":"99.99",
+            "price":"€99.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/marcjacobs.jpg"
+			"imageUrl":"./img/Zenske/marcjacobs.jpg"
         },
         {
 			"name":"O'Neill Shore",
-            "price":"109.99",
+            "price":"€109.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/onneilshore.jpg"
+			"imageUrl":"./img/Zenske/onneilshore.jpg"
         },
         {
 			"name":"Polaroid S",
-            "price":"64.99",
+            "price":"€64.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/polaroid.jpg"
+			"imageUrl":"./img/Zenske/polaroid.jpg"
         },
         {
 			"name":"Police Alias",
-            "price":"94.99",
+            "price":"€94.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/policealias.jpg"
+			"imageUrl":"./img/Zenske/policealias.jpg"
         },
         {
 			"name":"Police Aphrodite",
-            "price":"104.99",
+            "price":"€104.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/policeaphrodite.jpg"
+			"imageUrl":"./img/Zenske/policeaphrodite.jpg"
         },
         {
 			"name":"Police Mascara",
-            "price":"89.99",
+            "price":"€89.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/policemascara.jpg"
+			"imageUrl":"./img/Zenske/policemascara.jpg"
         },
         {
 			"name":" SuperDry",
-            "price":"54.99",
+            "price":"€54.99",
             "model":"zenski",
-			"imageUrl":"/img/Zenske/superdry.jpg"
+			"imageUrl":"./img/Zenske/superdry.jpg"
         },
         {
 			"name":"Esprit",
-            "price":"45.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/esprit.jpg"
+            "price":"€45.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/esprit.jpg"
         },
         {
 			"name":"Esprit Blue",
-            "price":"40.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/espritblue.jpg"
+            "price":"€40.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/espritblue.jpg"
         },
         {
 			"name":"Polaroid Black",
-            "price":"34.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/polaroidblack.jpg"
+            "price":"€34.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/polaroidblack.jpg"
         },
         {
 			"name":"Polaroid Green",
-            "price":"39.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/polaroidgreen.jpg"
+            "price":"€39.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/polaroidgreen.jpg"
         },
         {
 			"name":"Polaroid Ultraviolet",
-            "price":"49.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/polaroidultraviolet.jpg"
+            "price":"€49.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/polaroidultraviolet.jpg"
         },
         {
 			"name":"RayBan Junior",
-            "price":"89.99",
-            "model":"decije",
-			"imageUrl":"/img/Decije/raybanjunior1.jpg"
+            "price":"€89.99",
+            "model":"deciji",
+			"imageUrl":"./img/Decije/raybanjunior1.jpg"
         }
     ]
 }
